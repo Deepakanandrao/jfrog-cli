25346328751ee784df9acb5911fdf3858805fa35
Deep Anandrao <8665209+Deepakanandrao@users.noreply.github.com>
2021-01-02 17:08:49 -0500

    Initial commit JFrog cli connnect initial commit Readme
    Update action.yml
    
    Update action.yml
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update Dockerfile
    
    Update Dockerfile
    
    Update entrypoint.sh
    
    Update Dockerfile
    
    Update Dockerfile
    
    Update Dockerfile
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update action.yml
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update action.yml
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update action.yml
    
    Update action.yml
    
    Update entrypoint.sh
    
    Update action.yml
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update action.yml
    
    Update Dockerfile
    
    Update Dockerfile
    
    Update entrypoint.sh
    
    Update entrypoint.sh
    
    Update Dockerfile
    
    Update README.md
    
    Update Dockerfile
    
    Update README.md
    
    Update README.md
    
    Update Dockerfile
    
    Update README.md

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+if [ "$ACCESS_TYPE" = "username-password" ]
+then 
+    sh -c 'jfrog rt c "$SERVER_ID" --interactive=false --basic-auth-only=true --url="$SERVER_URL" --user="$USERNAME" --password="$PASSWORD"'
+fi
+
+if [ "$ACCESS_TYPE" = "api-key" ];
+then
+       sh -c 'jfrog rt c "$SERVER_ID" --interactive=false --basic-auth-only=true --url="$SERVER_URL" --apikey="$API_KEY"'
+fi
+
+if [ "$access_type" = "access-token" ]
+then 
+       sh -c 'jfrog rt c "$SERVER_ID" --interactive=false --basic-auth-only=true --url="$SERVER_URL" --access-token="$API_KEY"'
+fi
+
+for cmd in "$@"; do
+    echo "Running: '$cmd'"
+    if sh -c "$cmd"; then
+        echo "Success!"
+    else
+        exit_code=$?
+        echo "Failure: '$cmd' exited with $exit_code"
+        exit $exit_code
+    fi
+done
