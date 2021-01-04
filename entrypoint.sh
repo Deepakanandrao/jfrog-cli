dd63c94d785da825b87272eab3f20ca96c563627
Deep Anandrao <8665209+Deepakanandrao@users.noreply.github.com>
2021-01-04 17:55:07 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,22 +6,20 @@
 
 set -e
 
-if [$server_url && $access_type && (($username && $password) || $access_token)] ;
-then
-    if [$access_type = 'username-password'];
+
+if [$access_type = 'username-password'];
     then 
        sh -c "jfrog rt c $server_id --interactive=false --basic-auth-only=true --url=$server_url --user=$username --password=$password"
-     elif [$access_type == 'api-key'];
+fi
+if [$access_type == 'api-key'];
      then
        sh -c "jfrog rt c $server_id --interactive=false --basic-auth-only=true --url=$server_url --apikey=$api_key"
-    elif [$access_type = 'access-token'];
+fi
+if [$access_type = 'access-token'];
     then 
        sh -c "jfrog rt c $server_id --interactive=false --basic-auth-only=true --url=$server_url --access-token=$api_key"
-    fi
-else
-    echo "Please enter required parameters"
 fi
-
+    
 for cmd in "$@"; do
     echo "Running: '$cmd'"
     if sh -c "$cmd"; then
