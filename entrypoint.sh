208dcb162e62ffda9d6a7580072a40c0d6a0eae8
Deep Anandrao <8665209+Deepakanandrao@users.noreply.github.com>
2021-01-04 17:52:09 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,41 +6,15 @@
 
 set -e
 
-
-# $INPUT_SERVER_ID:
-
-# $INPUT_SERVER_URL:
-
-# $INPUT_DISTRIBUTION_URL:
-
-# $INPUT_ACCESS_TYPE:
-
-# $INPUT_USERNAME:
-
-# $INPUT_PASSWORD:
-
-# $INPUT_ACCESS_TOKEN:
-
-# $INPUT_API_KEY:
-
-# $INPUT_CLI_CMD:
-   
-
-# sudo apt install -y jfrog-cli
-if ! [$server_id]
-    then
-        $server_id = "JFrog Artifactory Server"
-fi
-
-if [$server_url && $access_type && (($username && $password) || $access_token)] 
+if [$server_url && $access_type && (($username && $password) || $access_token)] ;
 then
-    if [$access_type = 'username-password']
+    if [$access_type = 'username-password'];
     then 
        sh -c "jfrog rt c $server_id --interactive=false --basic-auth-only=true --url=$server_url --user=$username --password=$password"
-     elif [$access_type == 'api-key']
+     elif [$access_type == 'api-key'];
      then
        sh -c "jfrog rt c $server_id --interactive=false --basic-auth-only=true --url=$server_url --apikey=$api_key"
-    elif [$access_type = 'access-token']
+    elif [$access_type = 'access-token'];
     then 
        sh -c "jfrog rt c $server_id --interactive=false --basic-auth-only=true --url=$server_url --access-token=$api_key"
     fi
